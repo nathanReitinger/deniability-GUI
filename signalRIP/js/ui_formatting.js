d41f3825bda4503fb4e2c3a70caacabbb07f6bb7
Omer Akgul <omer@omers-MBP-2.fios-router.home>
2021-09-22 19:37:03 -0400

    resizing done, including notfications and first message

--- signalRIP/js/ui_formatting.js
+++ signalRIP/js/ui_formatting.js
@@ -1,4 +1,80 @@
 
+// resizes the message so that they don't overlap. also fixes the height parameter.
+function resize_message(message_div){
+
+    try{
+        lowest_message = find_prev_message(message_div)
+        lowest_message_loc = parseInt(lowest_message.style.top, 10)
+        last_message_data_row = lowest_message.getAttribute("data-row")
+        lowest_message_height = parseInt(lowest_message.style.height, 10)
+    } catch(e){
+        lowest_message_loc = 256-57
+        last_message_data_row = "0"
+        lowest_message_height = 57
+    }
+    
+
+    new_data_row = "" + ( parseInt(last_message_data_row, 10) + 1 )
+    message_div.setAttribute("data-row", new_data_row)
+
+    try{
+        message_height_offset = 9
+        message_div_height = message_div.getElementsByClassName("module-message__container-outer")[0].clientHeight + message_height_offset    
+
+    } catch (e){
+        message_height_offset = 41
+        message_div_height = message_div.getElementsByClassName("module-inline-notification-wrapper")[0].clientHeight + message_height_offset
+      
+    }
+
+  
+    message_div.style.height = message_div_height + "px"
+    message_div.style.top = (lowest_message_loc + lowest_message_height ) + "px"
+
+
+}
+
+
+
+
+function adjust_position(message_div){
+    lowest_message = find_prev_message(message_div)
+    lowest_message_loc = parseInt(lowest_message.style.top, 10)
+    message_div.style.top = (lowest_message_loc + parseInt(lowest_message.style.height, 10) ) + "px"
+
+}
+
+
+
+function resize_message_container(){
+    outer_container = document.getElementById("where_all_the_messages_go")
+
+    message_div = find_prev_message(null)
+
+    new_height = ( parseInt(message_div.style.top, 10) + parseInt(message_div.style.height, 10) ) 
+    new_height_offset =   40 // parseInt(outer_container.style.height, 10) - lowest_message_loc - parseInt(lowest_message.style.height)
+
+    outer_container.style.height = (new_height + new_height_offset)  + "px"
+    outer_container.style.setProperty("max-height", outer_container.style.height)
+}
+
+
+// gets message prior to the one given. If given sometihng we don't have yet, return the last message.
+function find_prev_message(message_div){
+    all_messages = document.getElementsByClassName("module-timeline__message-container")
+
+    prev_message = 0
+    for(message of all_messages){
+        if( message.contains(message_div)){
+            return(prev_message)
+        }
+        prev_message = message
+    }
+
+    return prev_message
+}
+
+
 
 
 function add_message(text_to_add, text_box){
@@ -8,29 +84,10 @@ function add_message(text_to_add, text_box){
     message_inners_incoming = `<div class="module-message module-message--incoming" tabindex="0" role="button"><div class="module-message__container-outer"><div class="module-message__container module-message__container--incoming"><div dir="auto" class="module-message__text module-message__text--incoming"><span contenteditable>Yes I'm&nbsp;here</span></div><div class="module-message__metadata module-message__metadata--incoming"><span class="module-message__metadata__date module-message__metadata__date--incoming" title="Mon, Sep 20, 2021 10:49 AM"><span contenteditable>1m</span></span><div class="module-expire-timer module-expire-timer--60 module-expire-timer--incoming"></div></div></div></div><div class="module-message__buttons module-message__buttons--incoming"><div role="button" class="module-message__buttons__react" aria-label="React to Message"></div><div role="button" aria-label="Reply to Message" class="module-message__buttons__reply module-message__buttons__download--incoming"></div><div class="react-contextmenu-wrapper"><div role="button" aria-label="More actions" class="module-message__buttons__menu module-message__buttons__download--incoming"></div></div></div></div>`
 
     all_messages = document.getElementsByClassName("module-timeline__message-container")
-    console.log(all_messages)
-
-
-    lowest_message_loc = 0
-    last_message_data_row = 0
-    lowest_message = null   
-    for( item of all_messages){
-        temp_top = parseInt(item.style.top, 10)
-        console.log(temp_top)
-        if(temp_top > lowest_message_loc){
-            lowest_message_loc = temp_top
-            last_message_data_row = item.getAttribute("data-row")
-            lowest_message = item
-        }
-    }
-
-    console.log(lowest_message_loc)
 
     // new message creation
     let message_div = document.createElement('div');
     message_div.setAttribute("id", "randomo_stiring")
-    new_data_row = "" + ( parseInt(last_message_data_row, 10) + 1 )
-    message_div.setAttribute("data-row", new_data_row)
     message_div.setAttribute("class", "module-timeline__message-container" )
     message_div.setAttribute("role", "row" )
     message_div.setAttribute("style", "height: 64px; left: 0px; position: absolute; top: 1779px; width: 446px;")
@@ -41,42 +98,40 @@ function add_message(text_to_add, text_box){
         message_div.innerHTML = message_inners_outgoing
     }
 
-    // message_div.innerHTML = message_inners_incoming
-
-
     text_displayed = message_div.getElementsByTagName("span")[0]
     console.log(text_displayed)
     text_displayed.innerHTML = text_to_add
 
-
     outer_container.appendChild(message_div)
 
-    message_div = outer_container.childNodes[outer_container.childNodes.length-1]
+    for(span of message_div.getElementsByTagName("span")){
+        span.addEventListener("keyup", resize_listener)
+    }
 
 
-    message_height_offset = 9
+    resize_message(message_div)
+    resize_message_container()
 
-    message_div_height = message_div.getElementsByClassName("module-message__container-outer")[0].clientHeight + message_height_offset
-    console.log("asdfasfasdf  " +  message_div_height)
-    message_div.style.height = message_div_height + "px"
-
-    message_div.style.top = (lowest_message_loc + parseInt(lowest_message.style.height, 10) ) + "px"
-
-    new_height = ( parseInt(message_div.style.top, 10) + parseInt(message_div.style.height, 10) ) 
-    new_height_offset =   40 // parseInt(outer_container.style.height, 10) - lowest_message_loc - parseInt(lowest_message.style.height)
-    console.log("lowest height", lowest_message.style.height)
-    console.log("new offset", new_height_offset)
-    outer_container.style.height = (new_height + new_height_offset)  + "px"
-    outer_container.style.setProperty("max-height", outer_container.style.height)
-
-
-    console.log(text_box)
-    text_box.innerHTML = ""
-    console.log(text_box.innerHTML)
 
     document.getElementsByClassName("timeline-placeholder")[0].setAttribute("style", "bottom: -3em;")
     message_div.scrollIntoView();
 
+}
+
+function resize_listener(event){
+
+    all_messages = document.getElementsByClassName("module-timeline__message-container")
+
+    found = false
+    for(message of all_messages){
+        if(message.contains(event.currentTarget)){
+            resize_message(message)
+            found=true
+        } else if(found){
+            adjust_position(message)
+        }
+    }
+    resize_message_container()
 }
 
 $(function(){
@@ -94,7 +149,7 @@ $(function(){
 
         if (event.which == 13 || event.keyCode == 13){
             add_message(text_box.innerText, text_box)
-            gotoBottom(document.getElementById("where_all_the_messages_go"))
+            //gotoBottom(document.getElementById("where_all_the_messages_go"))
         }
     });
 
@@ -112,7 +167,12 @@ $(function(){
         }
     });
 
+    // these are the message resizers. 
+    $("span").on("keyup", resize_listener);
+
+
 });
+
 
 
 
